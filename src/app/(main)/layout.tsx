1291dfe8e6e7c4777c836e2b42f214f1619f8895
Chris <k.ciach@dook.pro>
2024-07-18 21:10:41 +0200

    feat: Add lazy loading for RoomsSortingSelectWrapper component

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,6 +3,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 import SafeSuspense from "@/components/utilities/safe-suspense";
 
 import { RoomPaginationWrapper, RoomsSortingSelectWrapper } from "./wrappers";
+import { Suspense } from "react";
 
 async function AwaitedPagination() {
   const rooms = await apiFetch("/rooms");
@@ -13,7 +14,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div className="mb-4">
-        <RoomsSortingSelectWrapper />
+        <Suspense fallback={<Skeleton className="h-[32px] w-[250px]" />}>
+          <RoomsSortingSelectWrapper />
+        </Suspense>
       </div>
       {children}
       <SafeSuspense fallback={<Skeleton className="h-8 w-[100px]" />}>
