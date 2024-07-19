c595df0989a0819dbea83535469c68d48fd01136
Chris <k.ciach@dook.pro>
2024-07-19 13:11:48 +0200

    chore: cover NEXT_PUBLIC_API_URL error

--- src/api/apiFetch.ts
+++ src/api/apiFetch.ts
@@ -1,5 +1,11 @@
 import { API } from "./types";
 
+if (!process.env.NEXT_PUBLIC_API_URL) {
+  throw new Error(
+    "NEXT_PUBLIC_API_URL is not defined. Please define it in .env.local file based on .env.example file."
+  );
+}
+
 export default async function apiFetch<K extends keyof API>(url: K) {
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${url}`);
 
